3c20a3e8e684d414fcf07f407db705bedb461b27
ferdiand <ferdiand@gmail.com>
2022-09-16 18:21:39 +0000

    fds

--- custom_components/integration_fufopi/__init__.py
+++ custom_components/integration_fufopi/__init__.py
@@ -76,7 +76,7 @@ class VEDirectCoordinator(DataUpdateCoordinator):
     async def _async_update_data(self):
         """Update data via library."""
         try:
-            _buffer = self._serial.read_lines()
+            _buffer = self._serial.read_until("\r\n")
             self.logger.warning(_buffer)
             return _buffer
         except Exception as exception:
