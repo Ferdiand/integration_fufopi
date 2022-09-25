314cd6db0451beead7324b35733f206df19d28cf
ferdiand <ferdiand@gmail.com>
2022-09-25 16:20:54 +0000

    khuj

--- custom_components/integration_fufopi/sensor.py
+++ custom_components/integration_fufopi/sensor.py
@@ -37,9 +37,10 @@ class IntegrationBlueprintSensor(VEDirectEntity, SensorEntity):
         """Return the native value of the sensor."""
         _data = self.coordinator.data[self.key]
         if isinstance(_data["value"], Decimal):
-            decimal.getcontext().prec = 3
             _decimal = Decimal()
-            _decimal = _data["value"] * _data["unit_conversion"]
+            _decimal = (_data["value"] * _data["unit_conversion"]).quantize(
+                Decimal("1.000")
+            )
             # _decimal = _decimal.quantize(Decimal("1.000"))
             self.coordinator.logger.warning(f"Decimal value {self.key} ::: {_decimal}")
             return _decimal
